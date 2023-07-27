01f5dbb16bd08a68390677f60ec65f42911742e8
pawarisa <pawarisasangha15@gmail.com>
2023-07-27 12:45:12 +0700

    lab03

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -3,3 +3,13 @@
 //TODO: #4 display me "Hello World." to me.
 
 //TODO: #5 Write your private info into Student-Info.txt
+//TODO: #6 Fix all bugs.
+#include <stdio.h>
+
+int main() {
+    printf( "Hello, I'm Javis (RMUTL-Version)" );
+
+    
+    return '0' ;
+
+}
--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,8 +1,14 @@
 //TODO: #9 Fix bug and arrange code in CODEX pattern.
-#include <studio.h>
-#include <coni.h>
+#include <stdio.h>
+#include <conio.h>
 int main(){
-float a,b=3;bool _1=0;printf("%d ",_1);int i=0;
-if(_1!=0)printf("ok ")else while(i<=5) 
-printf(" not-okey-%d ", ++i) for(i=3;i<10;i+=1)if( i%2==(!1?1:2)) printf("see see"): printf("haha");
+float a,b=3;bool _1=0;
+printf("%d ",_1);int i=0;
+if(_1!=0)printf("ok ");
+else while(i<=5) 
+printf(" not-okey-%d ", ++i) ;
+for(i=3;i<10;i+=1)
+if( i%2==(!1?1:2)) 
+printf("see see");
+printf("haha");
 return 0;}
